c179f7bbb5126765292d329b35afcb23b9fab596
Akhlaq Malik <akhlaq87@gmail.com>
2015-05-05 22:13:46 +0200

    initial commit to the tanzeem member with some basic test #1

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,21 +1,34 @@
 class Member < ActiveRecord::Base
 
 # Assoziations
-	has_many :incomes
-	has_many :receipts
+  has_many :incomes
+  has_many :receipts
 
 # Methods
-	def full_name
-		"#{last_name}, #{first_name}"
-	end
+  def full_name
+    "#{last_name}, #{first_name}"
+  end
 
-	def current_income
-		incomes.order(starting_date: :desc).first
-	end
+  def current_income
+    incomes.order(starting_date: :desc).first
+  end
 
-	def age
-		now = Time.now.utc.to_date
-		now.year - date_of_birth.year - (date_of_birth.to_date.change(:year => now.year) > now ? 1 : 0)
-	end
+  def age
+    now = Time.now.utc.to_date
+    now.year - date_of_birth.year - (date_of_birth.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
+
+  def tanzeem
+    if age < 7 then
+      'Kind'
+    elsif age >= 7 && age <= 15
+      'Tifl'
+    elsif age > 15 && age < 40
+      'Khadim'
+    elsif age >= 40
+      'Nasir'
+    end
+
+  end
 
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
+require 'awesome_print'
 
 RSpec.describe Member, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'tanzeem' do
+
+    before(:each) do
+      @member = FactoryGirl.create(:member)
+      Timecop.freeze(Date.parse("01-05-2015")) # freeze Date to 01.05.2015
+    end
+
+    after(:each) do
+      #ap "age of member: #{@member.age} (#{@member.date_of_birth} - #{Date.today})"
+    end
+
+    it "should have a valid factory a valid tanzeem method" do
+      expect(@member).to be_valid
+      expect(@member).to respond_to(:tanzeem)
+    end
+
+    it "should return 'Kind' for age < 7" do
+      @member.date_of_birth = "2008-05-02"
+      expect(@member.tanzeem).to eq('Kind')
+    end
+
+    it "should return 'Tifl' for age >= 7" do
+      @member.date_of_birth = "2008-05-01"
+      expect(@member.tanzeem).to eq('Tifl')
+    end
+
+    it "should return 'Khadim' for age > 16 and < 40" do
+      @member.date_of_birth = "1998-01-01"
+      expect(@member.tanzeem).to eq('Khadim')
+    end
+
+    it "should return 'Nasir' for age > 41" do
+      @member.date_of_birth = "1974-05-01"
+      expect(@member.tanzeem).to eq('Nasir')
+    end
+
+    it "should return 'Tifl' if member turn 15 on the 01-11-2015" do
+      Timecop.freeze(Date.parse("01-11-2015"))
+      @member.date_of_birth = "2000-11-01"
+      expect(@member.tanzeem).to eq('Tifl')
+    end
+
+    it "should return 'Khadim' if member turn 15 on the 31-10-2015" do
+      Timecop.freeze(Date.parse("01-11-2015"))
+      @member.date_of_birth = "2000-10-31"
+      expect(@member.tanzeem).to eq('Khadim')
+    end
+
+  end
+
 end
