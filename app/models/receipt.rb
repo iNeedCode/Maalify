7879d9f8c44dffae02bf274c9fdef54e4e4d3dd6
Akhlaq Malik <akhlaq87@gmail.com>
2015-11-10 21:44:00 +0100

    added validation to receipt items and there time beloging to a budget fix #46

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -8,6 +8,8 @@ class Receipt < ActiveRecord::Base
 # Validations
   validates_associated :items, presence: true
   validates :receipt_id, uniqueness: true, presence: true
+  validate :budget_exists_for_date?
+  validate :single_donation_used_in_one_receipt
 
 
 # Methods
@@ -16,4 +18,26 @@ class Receipt < ActiveRecord::Base
     items.collect { |li| li.amount.present? ? sum += li.amount : 0 }
     sum
   end
+
+  private
+  def single_donation_used_in_one_receipt
+    unless items.map(&:donation_id).length == items.map(&:donation_id).uniq.length
+      errors.add(:items, "#{I18n.t('receipt.error.double_receipt_item')}")
+    end
+  end
+
+  def budget_exists_for_date?
+    all_member_budgets = Budget.where(member: member)
+    matches = nil
+    items.each do |item|
+      matches = all_member_budgets.select do |budget|
+        (budget.donation_id == item.donation_id) && (budget.start_date..budget.end_date).include?(date)
+      end
+    end
+
+    if matches.nil? || matches.empty?
+      errors.add(:items, "#{I18n.t('receipt.error.no_budget_period_available')}")
+    end
+  end
+
 end
