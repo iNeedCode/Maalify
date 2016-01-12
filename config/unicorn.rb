924ebd996dac3fbcb7f6441e37899d990cc9dc05
Akhlaq Malik <akhlaq87@gmail.com>
2016-01-12 18:28:31 +0100

    towards automatic restart of unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -19,20 +19,20 @@ stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 # Set master PID location
 pid "#{shared_dir}/tmp/pids/unicorn.pid"
 
-#
-# before_fork do |server, worker|
-#   ActiveRecord::Base.connection.disconnect!
-#
-#   old_pid = "#{server.config[:pid]}.oldbin"
-#   if File.exists?(old_pid) && server.pid != old_pid
-#     begin
-#       Process.kill("QUIT", File.read(old_pid).to_i)
-#     rescue Errno::ENOENT, Errno::ESRCH
-#       # someone else did our job for us
-#     end
-#   end
-# end
-#
-# after_fork do |server, worker|
-#   ActiveRecord::Base.establish_connection
-# end
+
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end
