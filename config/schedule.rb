fd2ae09f677912e450eddb6c69a7fa1417e4c1f8
Akhlaq Malik <akhlaq87@gmail.com>
2016-01-21 11:16:49 +0100

    updated whenever #65

--- config/schedule.rb
+++ config/schedule.rb
@@ -33,7 +33,6 @@ every 1.month, :at => 'January 20th 6:00am' do
 end
 
 
-every 1.day, :at => '11:10 am' do
-  command "gem install backup"
+every 5.minutes do
   command "backup perform -t production_backup"
 end
