c6ec6de953efbfe5cdbc31b915b84033a54c97bb
Akhlaq Malik <akhlaq87@gmail.com>
2016-01-31 10:46:56 +0100

    replaced hard coded jamaat with config var

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,7 +3,7 @@ class ReportMailer < ApplicationMailer
   def mail_to_subscribers(_report)
     @report = _report
     mail bcc: @report.emails,
-         subject: "[Darmstadt-City] #{t('reporter.title')}: #{@report.name}"
+         subject: "[#{Setting.jamaat}] #{t('reporter.title')}: #{@report.name}"
   end
 
 end
