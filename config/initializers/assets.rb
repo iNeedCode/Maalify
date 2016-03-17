98aed79bc353a496b3ea4ef202376fe620b1289b
Akhlaq Malik <akhlaq87@gmail.com>
2016-03-17 11:44:28 +0100

    added bar for monthly procedings

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
 Rails.application.config.assets.precompile += %w( email.css )
+Rails.application.config.assets.precompile += %w( jsapi.js )
