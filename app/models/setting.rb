0fb76c1b5e39cf1b2e7bd93529184bf3bdc09103
Akhlaq Malik <akhlaq87@gmail.com>
2016-03-19 13:46:43 +0100

    contact field in email for reporter has been update

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,5 +2,5 @@ class Setting < RailsSettings::CachedSettings
   defaults[:jamaat] = "Jamaat"
   defaults[:mail_contact_tag_for_male] = "Bitte hier Kontaktdaten für den jeweiligen Nazimeen aufnehmen, die Zuständig sind z.B. Sec. Maal. Das wird in der Email angezeigt."
   defaults[:mail_contact_tag_for_female] = "Bitte hier Kontaktdaten für den jeweiligen Nazima aufnehmen, die Zuständig sind z.B. Nazima Maal. Das wird in der Email für Frauen angezeigt angezeigt."
-  defaults[:monthly_mail] = 20
+  defaults[:monthly_mail] = 1
 end
